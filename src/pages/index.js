9fda773d793825b4558452d3d4b5b5465dacc541
saikiran2508 <gsaikiran502@gmail.com>
2021-08-21 21:42:29 +0530

    xl

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ products }) {
       
       <Header />
 
-      <main className=' max-w-screen-2xl mx-auto'>
+      <main className=' max-w-screen-xl mx-auto'>
         {/* Banner */}
         <Banner />
 
